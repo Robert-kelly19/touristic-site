25a2497b3656b3459281b87ea0f272fd04b18673
Robert Kelly <kellyhandy31@gmail.com>
2025-03-19 01:03:22 +0100

    full work waiting for first review

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/package" element={<Package />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery/>} />
+          <Route path="/contact" element={<Contact/>} />
         </Routes>
       </BrowserRouter>
     </>
