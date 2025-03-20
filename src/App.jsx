9ee16348c0fe05711e07a7adc5c72b92d084b647
Robert Kelly <kellyhandy31@gmail.com>
2025-03-20 09:45:17 +0100

    deleted package.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Package from "./pages/Package";
-import Gallery from "./pages/Gallery";
+import Gallery from "./pages/PicRoom";
 import Contact from "./pages/Contact";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/package" element={<Package />} />
-          <Route path="/gallery" element={<Gallery/>} />
-          <Route path="/contact" element={<Contact/>} />
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
+          <Route path="/package" element={<Package />}/>
+          <Route path="/gallery" element={<Gallery/>}/>
+          <Route path="/contact" element={<Contact/>}/>
         </Routes>
       </BrowserRouter>
     </>
